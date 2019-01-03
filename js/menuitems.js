5ea082c1743320d072cec2de1706e25fcb9ea647
andreitudos <andreitudos@gmail.com>
2019-01-03 16:45:04 +0000

    Navigation menu from array

--- js/menuitems.js
+++ js/menuitems.js
@@ -1,42 +1,39 @@
-function getMenuItems(){
-    var menuitem = [
-        'Home',
-        'Login',
-        'Register',
-        'Update User',
-        'Order',
-        'Logout',
-    ];
-    
-    function menuUL(array) {
-    // Create the list element:
-  // Create the list element:
-  let list = document.createElement('ul');
-  list.setAttribute('id','menubar');
-  list.setAttribute('class', 'menubar');
+function getMenuItems() {
+  var menuitem = [
+    "Home",
+    "Login",
+    "Register",
+    "Update User",
+    "Order",
+    "Logout"
+  ];
 
-  for(let i = 0; i < array.length; i++) {
+  function menuUL(array) {
+    // Create the list element:
+    let list = document.createElement("ul");
+    list.setAttribute("id", "menubar");
+    list.setAttribute("class", "menubar");
+
+    for (let i = 0; i < array.length; i++) {
       // Create the list item:
       let itemText = array[i];
-      let item = document.createElement('li');
-      let aItem = document.createElement('a');
-     
-      item.setAttribute('id', (itemText.replace(/\s/g, "")).toLowerCase());
-      item.setAttribute('onclick','setActive(this.id)');
-
+      let item = document.createElement("li");
+      let aItem = document.createElement("a");
+      aItem.appendChild(document.createTextNode(itemText));
+ 
+      item.setAttribute("id", itemText.replace(/\s/g, "").toLowerCase());
+      item.setAttribute("onclick", "setActive(this.id)");
 
       // Set its contents:
-      item.appendChild(document.createTextNode(aItem));
-      // item.appendChild(aItem);
+      item.appendChild(aItem);
 
       // Add it to the list:
       list.appendChild(item);
-  }
+    }
 
-  // Finally, return the constructed list:
-  return list;
-}
-    
-    // Add the contents of options[0] to #foo:
-    document.getElementById('navdiv').appendChild(menuUL(menuitem));
+    //Return the constructed list:
+    return list;
+  }
+  // Add the contents of options[0] to #foo:
+  document.getElementById("navdiv").appendChild(menuUL(menuitem));
 }
